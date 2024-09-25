d0362a2e6ccb588e42580374712e63ad2bfd9d04
Brennis Castro <castrocanobrennis@gmail.com>
2024-09-25 14:17:47 -0500

    vanilla js

--- main.js
+++
@@ -1,9 +0,0 @@
-const URL = "https://rickandmortyapi.com/api/character/";
-
-const fetchData = async () => {
-    const res = await fetch(URL);
-    const { results } = await res.json();
-
-    return results;
-};
-
